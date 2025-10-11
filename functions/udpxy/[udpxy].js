f9cb33a28479b2a3972f80f3a4e679950cec0da8
suzukua <suzukua>
2025-10-11 12:15:13 +0800

    增加udpxy动态地址转换

---
+++ functions/udpxy/[udpxy].js
@@ -0,0 +1,12 @@
+
+export async function onRequest(context) {
+    if (!context.params.udpxy){
+        return new Response(`udpxy参数错误，例：https://iptv.zsdc.eu.org/udpxy/192.168.100.1:4022`);
+    }
+    const response = await fetch(`https://iptv.zsdc.eu.org/home/udpxy_iptv.m3u8`, {
+        method: 'GET',
+    });
+    let m3uText = await response.text();
+    m3uText = m3uText.replaceAll("192.168.100.1:4022", context.params.udpxy)
+    return new Response(m3uText);
+}
