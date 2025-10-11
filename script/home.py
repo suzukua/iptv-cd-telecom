eb00c962ef630fe95297d0dcd15b3a6d254df638
suzukua <suzukua>
2025-10-11 16:56:18 +0800

    timeout=(10, 30)

--- script/home.py
+++ script/home.py
@@ -56,7 +56,7 @@ def findIcon(m, id):
 
 
 def loadIcon():
-    res = requests.get(sourceIcon51ZMT, verify=False, timeout=(10, 30)).content
+    res = requests.get(sourceIcon51ZMT, verify=False, timeout=(10, 60)).content
     m = []
     # res=""
     # with open('./index.html') as f:
@@ -144,7 +144,7 @@ def upload_convert_egp(m3u8_file, epg_m3u8_file):
         'myfile': ('iptv.m3u8', upload_file, 'audio/mpegurl')
     }
 
-    response = requests.post(url, headers=headers, files=files, verify=False, timeout=(10, 30))
+    response = requests.post(url, headers=headers, files=files, verify=False, timeout=(10, 60))
     print(response.text)
     upload_file.close()
     # 使用BeautifulSoup解析HTML
@@ -156,7 +156,7 @@ def upload_convert_egp(m3u8_file, epg_m3u8_file):
         # 获取绝对URL
         absolute_url = urljoin(urlparse(url).scheme + "://" + urlparse(url).hostname, file_url)
         # 下载文件
-        file_response = requests.get(absolute_url, verify=False, timeout=(10, 30))
+        file_response = requests.get(absolute_url, verify=False, timeout=(10, 60))
         # 将'your_file_name.extension'替换为所需的文件名和扩展名
         with open(epg_m3u8_file, 'w', encoding='utf-8') as file:
             file.write(file_response.content.decode())
@@ -197,7 +197,7 @@ def generateHome():
 # mIcons = loadIcon()
 # print("台标加载完成")
 print("开始加载频道")
-res = requests.get(sourceChengduMulticast, verify=False, timeout=(10, 30)).content
+res = requests.get(sourceChengduMulticast, verify=False, timeout=(10, 60)).content
 soup = BeautifulSoup(res, 'lxml')
 m = {}
 for tr in soup.find_all(name='tr'):
